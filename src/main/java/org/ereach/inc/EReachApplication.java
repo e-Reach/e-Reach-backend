22e6d386c5417b0eacfb5bec4db8aa0966600c3e
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-09-12 06:03:01 +0100

    initial commit

---
+++ src/main/java/org/ereach/inc/EReachApplication.java
@@ -0,0 +1,13 @@
+package org.ereach.inc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EReachApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EReachApplication.class, args);
+	}
+	
+}
