3de7e820ec09f58c71a715941f7068731e1bbca1
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-09-18 03:05:29 +0100

    created the pharmacist service

--- src/main/java/org/ereach/inc/services/users/EReachPharmacistService.java
+++ src/main/java/org/ereach/inc/services/users/EReachPharmacistService.java
@@ -1,4 +1,37 @@
 package org.ereach.inc.services.users;
 
+import lombok.AllArgsConstructor;
+import org.ereach.inc.data.dtos.request.AddMedicationRequest;
+import org.ereach.inc.data.dtos.request.CreatePractitionerRequest;
+import org.ereach.inc.data.dtos.request.UpdateEntryRequest;
+import org.ereach.inc.data.dtos.response.AddMedicationResponse;
+import org.ereach.inc.data.dtos.response.GetRecordResponse;
+import org.ereach.inc.data.dtos.response.PractitionerResponse;
+import org.ereach.inc.data.dtos.response.UpdateEntryResponse;
+import org.ereach.inc.data.repositories.users.EReachPractitionerRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class EReachPharmacistService implements PharmacistService{
+	private EReachPractitionerRepository practitionerRepository;
+	@Override
+	public PractitionerResponse createPharmacist(CreatePractitionerRequest practitionerRequest) {
+		return null;
+	}
+	
+	@Override
+	public AddMedicationResponse addMedication(AddMedicationRequest addMedicationRequest) {
+		return null;
+	}
+	
+	@Override
+	public GetRecordResponse viewPatientRecord(String patientIdentificationNumber) {
+		return null;
+	}
+	
+	@Override
+	public UpdateEntryResponse editEntry(UpdateEntryRequest updateEntryRequest) {
+		return null;
+	}
 }
