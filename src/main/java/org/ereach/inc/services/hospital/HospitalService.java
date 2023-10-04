96f84b6a60c3a051ef6d743721e02632d0e9d09c
Abdulmalik Alayande <alaabdulmalik03@gmail.com>
2023-10-04 17:10:04 +0100

    comms

--- src/main/java/org/ereach/inc/services/hospital/HospitalService.java
+++ src/main/java/org/ereach/inc/services/hospital/HospitalService.java
@@ -2,13 +2,12 @@ package org.ereach.inc.services.hospital;
 
 import org.ereach.inc.data.dtos.request.CreateHospitalRequest;
 import org.ereach.inc.data.dtos.request.UpdateHospitalRequest;
-import org.ereach.inc.data.dtos.response.GetHospitalAdminResponse;
-import org.ereach.inc.data.dtos.response.HospitalResponse;
-import org.ereach.inc.data.dtos.response.PractitionerResponse;
+import org.ereach.inc.data.dtos.response.*;
 import org.ereach.inc.data.dtos.response.entries.MedicalLogResponse;
 import org.ereach.inc.data.models.entries.MedicalLog;
 import org.ereach.inc.data.models.hospital.Record;
 import org.ereach.inc.data.models.users.Practitioner;
+import org.ereach.inc.exceptions.EReachBaseException;
 import org.ereach.inc.exceptions.FieldInvalidException;
 import org.ereach.inc.exceptions.RequestInvalidException;
 
@@ -23,14 +22,16 @@ public interface HospitalService {
 	HospitalResponse findHospitalByHefamaaId(String hefamaa_Id);
 	List<GetHospitalAdminResponse> findAllAdminByHefamaaId(String hospitalHefamaaId);
 	List<GetHospitalAdminResponse> findAllAdminByHospitalEmail(String hospitalEmail);
-	List<PractitionerResponse> getAllPractitioners(String hospitalEmail);
+	List<GetPractitionerResponse> getAllPractitioners(String hospitalEmail);
+	List<GetRecordResponse> getAllRecordsCreated(String hospitalEmail) throws EReachBaseException;
 	HospitalResponse viewHospitalProfileByEmailOrHefamaaId(String email, String hefamaaId);
+	
 	List<HospitalResponse> getAllHospitals();
-	
 	HospitalResponse findHospitalById(String id);
-	String removeHospital(String mail) throws RequestInvalidException;
 	
+	String removeHospital(String mail) throws RequestInvalidException;
 	MedicalLogResponse addToLog(String hospitalEmail, MedicalLog medicalLog);
+	
 	void addToRecords(String hospitalEmail, Record savedRecord);
 	
 	List<MedicalLogResponse> viewPatientsMedicalLogs(String hospitalEmail);
